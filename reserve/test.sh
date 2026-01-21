09ca23f525b0cb5b9e1594f413c58a84c5a166eb
Jiri Jaburek <comps@nomail.dom>
2026-01-21 13:33:51 +0100

    upgrade CentOS Stream to latest packages
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- reserve/test.sh
+++ reserve/test.sh
@@ -147,6 +147,15 @@ echo -n > /etc/tmpfiles.d/restraint.conf
 
 # ------------------------------------------------------------------------------
 
+# upgrade to latest available packages from each given stream
+# - this fixes issues with outdated packages, and overall isn't that big,
+#   typically 10-30 packages are upgraded (CS) or even 0 (Fedora)
+if [[ $os_id == centos ]]; then
+    dnf upgrade -y --skip-broken || true
+fi
+
+# ------------------------------------------------------------------------------
+
 # install SSH key
 if [[ $RESERVE_SSH_PUBKEY ]]; then
     mkdir -p ~/.ssh
